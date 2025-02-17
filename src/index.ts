01a946b59863a2109001c9b235048fbab341d56d
elianismedina <elianismedina05@outlook.com>
2025-02-16 21:38:25 -0500

    added post webhook

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Hono, Context } from "hono";
 
 interface Env {
   WEBHOOK_VERIFY_TOKEN: string;
+  GRAPH_API_TOKEN: string;
+  OPENAI_API_KEY: string;
 }
 
 const app = new Hono<{ Bindings: Env }>();
@@ -23,6 +25,40 @@ app.get("/webhook", (c) => {
   }
   c.status(403);
   return c.text("Invalid request");
+});
+app.post("/webhook", async (c) => {
+  const fbToken = c.env.GRAPH_API_TOKEN;
+  const openAIToken = c.env.OPENAI_API_KEY;
+
+  const body = await c.req.json();
+
+  const message = body.entry?.[0]?.changes[0]?.value.messages?.[0];
+
+  if (message?.type === "text") {
+    const business_phone_number_id =
+      body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+
+    const url = `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${fbToken}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    await fetch(url, {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        messaging_product: "whatsapp",
+        to: message.from,
+        text: { body: message.text.body },
+        context: {
+          message_id: message.id,
+        },
+      }),
+    });
+  }
+  c.status(200);
+  return c.text("ok");
 });
 
 export default app;
